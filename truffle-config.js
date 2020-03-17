8714e8f0032436d7ae374c1e1be2672ece5d5799
Alex Roan <alex.roan@hotmail.com>
2020-03-17 11:59:34 +0000

    Final deployment to Kovan

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKey = "private-key-goes-here";
+const endpointUrl = "endpoint-goes-here";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "5777",
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          //private keys array
+          [privateKey],
+          //url to ethereum node
+          endpointUrl
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
     }
   }
 }
